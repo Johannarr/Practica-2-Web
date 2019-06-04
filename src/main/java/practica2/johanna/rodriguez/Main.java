7f36225c4978af2f69b7517267796eea36171765
Johanna Rod <39653403+yohannarr@users.noreply.github.com>
2019-06-04 18:34:15 -0400

    Creando templates

--- src/main/java/practica2/johanna/rodriguez/Main.java
+++ src/main/java/practica2/johanna/rodriguez/Main.java
@@ -1,4 +1,23 @@
 package practica2.johanna.rodriguez;
 
+import freemarker.template.Configuration;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.staticFiles;
+import static spark.Spark.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        staticFiles.location("/templates");
+
+
+    }
 }
